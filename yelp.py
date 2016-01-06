d6788c9a17c417bf0afcca9682f6a37c67b96691
mariahkrim <mariahkrim@gmail.com>
2016-01-05 23:55:33 -0500

    Update yelp.py

--- yelp.py
+++ yelp.py
@@ -1,5 +1,3 @@
-
-
 # Written by Mariah Krimchansky, Nicole Gonzalez, Shreyas Lakhtakia
 # Last updated: 12/11/15, 11pm
 # ELE/COS 381
@@ -8,6 +6,7 @@
 import json
 import sys
 import matplotlib.pyplot as plt
+from scipy import stats
 
 # READ IN DATA -----------------------------------------------------------------
 
@@ -46,7 +45,7 @@ with open('yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_busines
     for line in f:
         tmpdata = (json.loads(line))
         #make sure business is a restaurant
-        if "Restaurants" in tmpdata["categories"] or "Food" in tmpdata["categories"]:
+        if ("Food" in tmpdata["categories"] or "Restaurants" in tmpdata["categories"]):
             #make sure business is in pittsburgh
             #right now changed to pennsylvania for more data..
             if " PA " in tmpdata["full_address"]:
@@ -266,6 +265,9 @@ zipTotalRestList = []
 zipLoAveRest = []
 zipMedAveRest = []
 zipHiAveRest = []
+zipLoNoZero = []
+zipMedNoZero = []
+zipHiNoZero = []
 
 #restaurant density, divided by their expensiveness
 zipLoDenRest = []
@@ -285,9 +287,15 @@ for key in zipAveInc:
         zipBayRestList.append(zipBayRest[str(key)])
         zipTotalRestList.append(zipTotalRest[str(key)])
         
-        zipLoAveRest.append(zipAvePriRest[str(key)][0])
-        zipMedAveRest.append(zipAvePriRest[str(key)][1])
-        zipHiAveRest.append(zipAvePriRest[str(key)][2])
+        if zipAvePriRest[str(key)][0] != 0:
+            zipLoAveRest.append(zipAvePriRest[str(key)][0])
+            zipLoNoZero.append(zipAveInc[key])
+        if zipAvePriRest[str(key)][1] != 0:
+            zipMedAveRest.append(zipAvePriRest[str(key)][1])
+            zipMedNoZero.append(zipAveInc[key])
+        if zipAvePriRest[str(key)][2] != 0:
+            zipHiAveRest.append(zipAvePriRest[str(key)][2])
+            zipHiNoZero.append(zipAveInc[key])
         
         zipLoDenRest.append(zipTotPriRest[str(key)][0])
         zipMedDenRest.append(zipTotPriRest[str(key)][1])
@@ -295,46 +303,93 @@ for key in zipAveInc:
 
 # plotting average restaurant rating against average income
 fig1 = plt.figure(1)
+
+slope, interc, r_value, p_value, std_err = stats.linregress(zipAveIncList,zipAveRestList)
 plt.plot(zipAveIncList, zipAveRestList, 'ro')
+lineval= [slope * min(zipAveIncList) + interc, slope * max(zipAveIncList) + interc]
+plt.plot([min(zipAveIncList), max(zipAveIncList)], lineval)
+plt.text(60000,4.8,"r_value = " + str(round(r_value,3)))
 plt.xlabel('Average Income, by ZIP')
 plt.ylabel('Average Restaurant Rating, by ZIP')
 plt.show()
 
 # plot average restaurant Bayesian rating vs average rating
 fig2 = plt.figure(2)
+
+slope, interc, r_value, p_value, std_err = stats.linregress(zipAveRestList,zipBayRestList)
 plt.plot(zipAveRestList, zipBayRestList, 'bo')
+lineval= [slope * min(zipAveRestList) + interc, slope * max(zipAveRestList) + interc]
+plt.plot([min(zipAveRestList), max(zipAveRestList)], lineval)
+plt.text(4.4,3.620,"r_value = " + str(round(r_value,3)))
 plt.xlabel('Average Bayesian Rating, by ZIP')
 plt.ylabel('Average Restaurant Rating, by ZIP')
 plt.show()
 
 # plot Bayesian average restaurant rating against average income
 fig3 = plt.figure(3)
+
+slope, interc, r_value, p_value, std_err = stats.linregress(zipAveIncList,zipBayRestList)
 plt.plot(zipAveIncList, zipBayRestList, 'go')
+lineval= [slope * min(zipAveIncList) + interc, slope * max(zipAveIncList) + interc]
+plt.plot([min(zipAveIncList), max(zipAveIncList)], lineval)
+plt.text(60000,3.620,"r_value = " + str(round(r_value,3)))
 plt.xlabel('Average Income, by ZIP')
 plt.ylabel('Average Bayesian Rating, by ZIP')
 plt.show()
 
 # plot total restaurants against average income
 fig4 = plt.figure(4)
+
+slope, interc, r_value, p_value, std_err = stats.linregress(zipAveIncList,zipTotalRestList)
 plt.plot(zipAveIncList, zipTotalRestList, 'go')
+lineval= [slope * min(zipAveIncList) + interc, slope * max(zipAveIncList) + interc]
+plt.plot([min(zipAveIncList), max(zipAveIncList)], lineval)
+plt.text(60000,250,"r_value = " + str(round(r_value,3)))
 plt.xlabel('Average Income, by ZIP')
 plt.ylabel('Number of Restaurants, by ZIP')
 plt.show()
 
 # plot average score, price wise
 fig5 = plt.figure(5)
-plt.plot(zipAveIncList, zipLoAveRest, 'go', color='blue')
-plt.plot(zipAveIncList, zipMedAveRest, 'go', color='green')
-plt.plot(zipAveIncList, zipHiAveRest, 'go', color='red')
+slope1, interc1, r_value1, p_value1, std_err1 = stats.linregress(zipLoNoZero,zipLoAveRest)
+slope2, interc2, r_value2, p_value2, std_err2 = stats.linregress(zipMedNoZero,zipMedAveRest)
+slope3, interc3, r_value3, p_value3, std_err3 = stats.linregress(zipHiNoZero,zipHiAveRest)
+plt.plot(zipLoNoZero, zipLoAveRest, 'go', color='blue', label='Low Price Range')
+plt.plot(zipMedNoZero, zipMedAveRest, 'go', color='green', label='Med Price Range')
+plt.plot(zipHiNoZero, zipHiAveRest, 'go', color='red', label='High Price Range')
+lineval1= [slope1 * min(zipAveIncList) + interc1, slope1 * max(zipAveIncList) + interc1]
+lineval2= [slope2 * min(zipAveIncList) + interc2, slope2 * max(zipAveIncList) + interc2]
+lineval3= [slope3 * min(zipAveIncList) + interc3, slope3 * max(zipAveIncList) + interc3]
+plt.plot([min(zipAveIncList), max(zipAveIncList)], lineval1,color='blue')
+plt.plot([min(zipAveIncList), max(zipAveIncList)], lineval2,color='green')
+plt.plot([min(zipAveIncList), max(zipAveIncList)], lineval3,color='red')
 plt.xlabel('Average Income, by ZIP')
 plt.ylabel('Average Score, by ZIP')
+legend = plt.legend(loc='upper left', shadow=True)
+for label in legend.get_texts():
+    label.set_fontsize('small')
 plt.show()
 
-# plot average score, price wise
+# plot density, price wise
+
+
 fig6 = plt.figure(6)
-plt.plot(zipAveIncList, zipLoDenRest, 'go', color='blue')
-plt.plot(zipAveIncList, zipMedDenRest, 'go', color='green')
-plt.plot(zipAveIncList, zipHiDenRest, 'go', color='red')
+
+slope1, interc1, r_value1, p_value1, std_err1 = stats.linregress(zipAveIncList,zipLoDenRest)
+slope2, interc2, r_value2, p_value2, std_err2 = stats.linregress(zipAveIncList,zipMedDenRest)
+slope3, interc3, r_value3, p_value3, std_err3 = stats.linregress(zipAveIncList,zipHiDenRest)
+plt.plot(zipAveIncList, zipLoDenRest, 'go', color='blue',label='Low Price Range')
+plt.plot(zipAveIncList, zipMedDenRest, 'go', color='green',label='Med Price Range')
+plt.plot(zipAveIncList, zipHiDenRest, 'go', color='red',label='High Price Range')
+lineval1= [slope1 * min(zipAveIncList) + interc1, slope1 * max(zipAveIncList) + interc1]
+lineval2= [slope2 * min(zipAveIncList) + interc2, slope2 * max(zipAveIncList) + interc2]
+lineval3= [slope3 * min(zipAveIncList) + interc3, slope3 * max(zipAveIncList) + interc3]
+plt.plot([min(zipAveIncList), max(zipAveIncList)], lineval1,color='blue')
+plt.plot([min(zipAveIncList), max(zipAveIncList)], lineval2,color='green')
+plt.plot([min(zipAveIncList), max(zipAveIncList)], lineval3,color='red')
 plt.xlabel('Average Income, by ZIP')
 plt.ylabel('Number of Restaurants, by ZIP')
+legend = plt.legend(loc='upper left', shadow=True)
+for label in legend.get_texts():
+    label.set_fontsize('small')
 plt.show()
